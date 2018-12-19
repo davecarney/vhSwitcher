3703f277a212e68976aece64843da8e5ee63738d
Dave Carney <cctvdave@gmail.com>
2018-12-19 12:59:40 -0600

    added two classes for setting full height images automatically. .full-height will set an element to the full height of the window. .full-mobile will set an element to full height minus the header, only works for mobile

--- README.md
+++ README.md
@@ -8,5 +8,14 @@ The script sets those elements' heights using jQuery.
 
 Conditionals are in place to ignore "toolbar hiding" .resize() events that fire upon scrolling in mobile browsers.
 
+Additionally, use a .full-height class to set an element to the full available screen height.
+
+Useful for when dealing with toolbar heights.
+
+If you have a header with a set height, you can use .full-mobile to set an element to the remaining space
+not taken up by the header. You will need to give the header a class of .header-adj so the plugin knows
+which element to measure and calculate.
+ONLY WORKS FOR MOBILE. Testing has shown that iOS will give unreliable heights for the header in iPads.
+
 ### Dependencies ###
 jQuery 1.7
--- vhSwitcher.js
+++ vhSwitcher.js
@@ -1,5 +1,5 @@
 /*
-* vhSwitcher() 1.0.0
+* vhSwitcher() 1.2.2
 *
 * Copyright 2018
 *
@@ -9,6 +9,14 @@
 * 	For elements styled using vh units, add a .vh-switcher class to them
 *	The script sets those elements' heights using javascript, with conditionals
 * 	to ignore "toolbar hiding" .resize() events in mobile browsers
+*
+*	Additionally, use a .full-height class to set an element to the full available screen height
+*	Useful for when dealing with mobile browser toolbar heights
+*
+*	Also, if you have a header with a set height, you can use .full-mobile to set an element to the remaining space
+*	not taken up by the header. You will need to give the header a class of .header-adj so the plugin
+*	knows which element to measure and calculate.
+*	ONLY WORKS FOR MOBILE. iOS gives unreliable header measurements for iPads.
 *
 */
 
@@ -22,6 +30,20 @@ jQuery(document).ready(function($) {
 			var thisHeight = $(this).innerHeight();
 			$(this).css('height', thisHeight);
 		});
+
+		var availableScreen = $(window).height();
+		$('.full-height').each(function() {
+			$(this).css('height', '');
+			$(this).css('height', availableScreen);
+		});
+
+		if ($(window).width() < 768) {
+			var leftOver = availableScreen - $('.header-adj').outerHeight();
+			$('.full-mobile').each(function() {
+				$(this).css('height', '');
+				$(this).css('height', leftOver);
+			});
+		}
 
 	}
 
